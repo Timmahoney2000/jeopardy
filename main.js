869b0699a046b598e74019628e5dab2eea76701c
Tim M <timmahoney2000@gmail.com>
2023-06-14 21:14:56 -0400

    wrapping up

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 initBoard()
 initCatRow()
 
+document.querySelector('button').addEventListener('click', buildCategories)
+
+// create category row
 function initCatRow() {
     let catRow = document.getElementById('category-row')
 
@@ -12,6 +15,7 @@ function initCatRow() {
     }
 }
 
+// create clue board
 function initBoard() {
    let board = document.getElementById('clue-board')
 
@@ -34,6 +38,121 @@ function initBoard() {
    }
 }
 
-function getClue() {
-    console.log('have a nice day')
+// call API
+function randInt() {
+    return Math.floor(Math.random() * (18418) + 1)
+}
+
+let catArray = []
+
+function buildCategories() {
+
+    if(!(document.getElementById('category-row').firstChild.innerText == '')) {
+        resetBoard()
+    }
+
+    const fetchReq1 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq2 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq3 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq4 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq5 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const fetchReq6 = fetch(
+        `https://jservice.io/api/category?&id=${randInt()}`
+    ).then((res) => res.json());
+
+    const allData = Promise.all([fetchReq1, fetchReq2, fetchReq3, fetchReq4, fetchReq5, fetchReq6])
+
+    allData.then((res) => {
+        console.log(res)
+        catArray = res
+        setCategories(catArray)
+    })
+
+}
+
+// reset board and dollar amount if needed
+
+function resetBoard() {
+    let clueParent = document. getElementById('clue-board')
+    while (clueParent.firstChild) {
+        clueParent.removeChild(clueParent.firstChild)
+    }
+    let catParent = document.getElementById('category-row')
+    while (catParent.firstChild) {
+        catParent.removeChild(catParent.firstChild)
+    }
+    document.getElementById('score').innerText = 0
+    initBoard()
+    initCatRow()
+}
+
+// load categories to the board
+function setCategories(catArray) {
+    let element = document.getElementById('category-row')
+        let children = element.children;
+        for(let i = 0; i < children.length; i++) {
+            children[i].innerHTML = catArray[i].title
+        }
+}
+
+// figure out which item was clicked
+function getClue (event) {
+    let child = event.currentTarget
+    child.classList.add('clicked-box')
+    let boxValue = child.innerHTML.slice(1)
+    let parent = child.parentNode
+    let index = Array.prototype.findIndex.call(parent.children, (c) => c === child)
+    let cluesList = catArray[index].clues
+    let clue = cluesList.find(obj => {
+        return obj.value == boxValue
+    })
+    
+    showQuestion(clue, child, boxValue)
+}
+
+// show question to user
+
+function showQuestion(clue, target, boxValue) {
+    let userAnswer = prompt(clue.question).toLowerCase()
+    let correctAnswer = clue.answer.toLowerCase().replace(/<\/[^>]+(>|$)/g, "")
+    let possiblePoints = +(boxValue)
+    target.innerHTML = clue.answer
+    target.removeEventListener('click',getClue,false)
+    evaluateAnswer(userAnswer, correctAnswer, possiblePoints)
+}
+
+
+function evaluateAnswer(userAnswer, correctAnswer, possiblePoints) {
+    let checkAnswer = (userAnswer == correctAnswer) ? 'correct' : 'incorrect'
+    let confirmAnswer = 
+    confirm(`For ${possiblePoints}, you answered "${userAnswer}", and the correct answer was "${correctAnswer}". Your answer appears to be ${checkAnswer}. Click OK to accept or click Cancel if the answer was not properly evaluated.`)
+    awardPoint(checkAnswer,confirmAnswer,possiblePoints)
+}
+
+// award points
+
+function awardPoints(checkAnswer, confirmAnswer, possiblePoints) {
+    if (!(checkAnswer == 'incorrect' && confirmAnswer == true)) {
+        let target = document.getElementById('score')
+        let currentScore = +(target.innerText)
+        currentScore += possiblePoints
+        target.innerText = currentScore
+    } else {
+        alert ('no points awarded')
+    }
 }
